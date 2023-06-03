c2ebb80d56be0e067d97b12d1070eff6415a7063
SylvinIsamaza <isamazasylvin@gmail.com>
2023-06-03 06:28:21 +0200

    updated

--- youtubeclone/src/services/auth.js
+++ youtubeclone/src/services/auth.js
@@ -1,30 +1,28 @@
-import axios from 'axios'
-import{loginSuccess,loginFailure} from '../redux/userState'
+import axios from 'axios';
+import { loginSuccess, loginFailure } from '../redux/userState';
+import { useDispatch } from 'react-redux';
 
-import { useDispatch} from 'react-redux';
+const baseUrl = 'http://localhost:8800';
 
-const baseUrl='http://localhost:8800'
-
-export  function createUser(data,setsnackmsg){
-    axios.post(baseUrl+`/api/auth/signUp`,data)
-    .then((data)=>{
-        console.log(data.data.message)
-       
-      setsnackmsg(data.data.message)
+export function createUser(data, setSnackMsg) {
+  axios.post(baseUrl + '/api/auth/signUp', data)
+    .then((response) => {
+      console.log(response.data.message);
+      setSnackMsg(response.data.message);
     })
-    .catch(err=>{
-        console.log('error while creating user'+err)
-        return(err)
-    }
-      
-        )
-}
-export  async function login(data){
-    let dispatch=useDispatch()
-    console.log(data)
-    axios.post(baseUrl+'/api/auth/signin',data)
-    .then((data)=>dispatch(loginSuccess(data.data)))
-    .catch(err=>dispatch(loginFailure())
-    )
+    .catch(err => {
+      console.log('Error while creating user: ' + err);
+      return err;
+    });
 }
 
+export async function login(data) {
+  const dispatch = useDispatch();
+  console.log(data);
+  try {
+    const response = await axios.post(baseUrl + '/api/auth/signin', data);
+    dispatch(loginSuccess(response.data));
+  } catch (err) {
+    dispatch(loginFailure());
+  }
+}
